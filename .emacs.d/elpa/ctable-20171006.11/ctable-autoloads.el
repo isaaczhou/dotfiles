064b33f3b2d40243e9398767f44e4c486630626f
isaaczhou <isaaczhou85@gmail.com>
2019-03-07 05:02:24 -0500

    updated .emacs

---
+++ .emacs.d/elpa/ctable-20171006.11/ctable-autoloads.el
@@ -0,0 +1,15 @@
+;;; ctable-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("ctable.el") (23657 43148 416989 411000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ctable-autoloads.el ends here
